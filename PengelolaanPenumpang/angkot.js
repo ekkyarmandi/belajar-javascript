38326ccc808a48b3070aadeff1d251de7dc18976
Ekky Armandi <ekkyarmandi@gmail.com>
2022-04-11 10:28:30 +0700

    adding function turunkanPenumpang

--- PengelolaanPenumpang/angkot.js
+++ PengelolaanPenumpang/angkot.js
@@ -1,5 +1,5 @@
-var penumpang = ['Ekky',undefined,'Shofi'];
-var max_slots = 15;
+var penumpang = [];
+var max_slots = 14;
 
 var tambahPenumpang = function(nama,penumpang){
     var sudah_turun = false;
@@ -9,26 +9,35 @@ var tambahPenumpang = function(nama,penumpang){
     for(var i = 0; i < penumpang.length; i++){
         if(penumpang[i]==nama){
             sudah_naik = true;
+            break
         } else if (penumpang[i]==undefined){
             next_slot = i;
             sudah_turun = true;
+            break
         }
     }
 
     if(sudah_naik){
         // test case 3 penumpang dengan nama yang sama dilarang naik
         console.log("Penumpang dengan nama " + nama + " sudah ada didalam angkot!");
-        return penumpang;
     } else if (sudah_turun){
         // test case 2 penumpang berikutnya diisi kedalam slot penumpang yang sudah turun
         penumpang[next_slot] = nama;
-        return penumpang;
     } else if(penumpang.length < max_slots){
         penumpang.push(nama);
-        return penumpang;
     } else {
         console.log("Angkot penuh!");
-        return penumpang;
     }
+    return penumpang;
+}
 
+var turunkanPenumpang = function(nama,penumpang){
+    for(var i = 0; i < penumpang.length; i++){
+        if(penumpang[i]==nama){
+            penumpang[i] = undefined;
+            return penumpang;
+        }
+    }
+    console.log(nama + " tidak ada sebagai penumpang");
+    return penumpang;
 }
